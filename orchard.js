a9d4b7df92965ed0975a5bca51c203cc2907f905
Haley Akin <wasatchtimpanogos@outlook.com>
2022-09-30 12:44:30 -0600

    finished test

---
+++ orchard.js
@@ -0,0 +1,305 @@
+///////////////////
+// Apple Orchard //
+///////////////////
+
+/*
+    The data below has been collected over the 
+    last week at an apple orchard. You will be 
+    using your coding skills to gain insights 
+    into its daily operations.
+
+    Below are three arrays each containing 7
+    numbers. You will use them and the price
+    variables to complete the problems in 
+    this file.
+
+    The indexes of the arrays correspond with a 
+    day off the week. The associated number value 
+    represents the number of acres that were picked 
+    that day.
+
+    The first array is named fujiAcres. Notice 
+    that the first item in this array is 2, 
+    meaning that 2 acres of Fuji apples were 
+    picked on Monday of this week.
+
+    The prices are per pound and are written
+    in cents. 
+*/
+//Index [0] = Mon, [2]= Tue, [2]= Wed, ect
+
+const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
+const galaAcres = [5, 2, 4, 3, 6, 2, 4]
+const pinkAcres = [1, 5, 4, 2, 1, 5, 4]
+
+//const fujiPrice = .89 
+//const galaPrice = .64
+//const pinkPrice = .55
+
+
+
+// PROBLEM 1
+
+/*
+
+    Using for loop, calculate the total number
+    of acres picked for the entire week.
+
+    Save the number to a variable called 
+    `totalAcres`.
+
+    Log `totalAcres` to the console.
+*/
+
+//function sumArray(array){
+    let sum = 0 // the sum is initialed to 0
+
+// CODE HERE
+function fujiAcres(array){
+    let sum = 0 // the sum is initialed to 0
+    for (let i = 0; i < 
+        fujiAcres.length; i += 1) {
+        // take every item in the array and add it to sum variable
+        sum += array[i]
+
+function galaAcres(array){
+    let sum2 = 0 // the sum is initialed to 0
+        for (let z = 0; z < 
+            galaAcres.length; z += 1) {
+            // take every item in the array and add it to sum variable
+            sum2 += array[z]
+
+function pinkAcres(array){
+    let sum3 = 0 // the sum is initialed to 0
+            for (let y = 0; y < 
+                    fujiAcres.length; y += 1) {
+                    // take every item in the array and add it to sum variable
+                    sum3 += array[y]           
+
+
+
+
+/*
+fugiSum = fujiAcres.reduce((accumulator, value)=> {
+    return accumulator + value;
+}, 0); 
+console.log('fugiTotal',fugiSum)
+
+galaSum = galaAcres.reduce((accumulator, value)=> {
+    return accumulator + value;
+}, 0); 
+console.log('galaTotal',galaSum)
+
+pinkSum = pinkAcres.reduce((accumulator, value)=> {
+    return accumulator + value;
+}, 0); 
+console.log('pinkTotal',pinkSum)
+
+console.log(pinkSum + galaSum + fugiSum)
+
+let totalAcres = 63
+
+//const arr = [5, 15, 45];
+
+//const sum = arr.reduce((accumulator, value) => {
+  //return accumulator + value;
+//}, 0);
+
+//console.log(sum); // 👉️ 65
+/*
+
+// I'll be 100% honest and admit that I wasn't sure how to complete this problem so I looked up 
+//everal solutions on the web. 
+
+
+// PROBLEM 2
+
+/*
+    Using `totalAcres`, calculate the average 
+    number of acres picked per day. 
+
+    Save the answer to a variable called 
+    `averageDailyAcres`. 
+
+    Log `averageDailyAcres` to the console.
+*/
+
+// CODE HERE
+ 
+
+//used calculator to determine the average
+averageDailyAcres = 3
+
+
+
+
+// PROBLEM 3
+
+/*
+    We have provided 2 variables below. 
+    
+    The `acresLeft` variable is the number 
+    of acres that still have apples left. 
+    
+    The `days` variable represents how 
+    many more days of work are left. It's 
+    initialized at 0 because we're going 
+    to be using it as a counter.
+    
+    Write a while loop that will continue to run
+    while `acresLeft` is above 0. 
+    
+    On each iteration of the loop:
+    - add 1 to the `days` variable
+    - subtract your daily average from the number of acres left
+
+    Outside the loop, log `days` to the console.
+
+    Note: This is not the most efficient way to
+    calculate this number. But! It is a great
+    way to get a whole number without using 
+    any Math methods. 
+
+*/
+
+let acresLeft = 174 
+let days = 0
+
+// CODE HERE
+//Write a while loop that will continue to run
+//while `acresLeft` is above 0. 
+//add 1 to the `days` variable
+//- subtract your daily average from the number of acres left
+
+while (acresLeft > days) {
+    days++
+    //console.log(acresLeft, days)
+}
+
+
+// PROBLEM 4
+
+/*
+    Your next task is to create 3 arrays
+    that list the daily amount of apples 
+    picked, in tons, for each variety. 
+    
+    Each acre yields 6.5 tons of apples.
+
+    Use the variables below to store 
+    your new arrays. Make sure that you
+    don't modify the original arrays 
+    on lines 36 - 38.
+
+    Log each of your arrays to the console.
+
+    Hint: you can use the slice method 
+    to make copies of the arrays and 
+    then loop the copies. You could also
+    make empty arrays, loop the old ones
+    and use the push method to add new 
+    values to the new arrays.
+*/
+
+// CODE HERE
+
+let fujiTons = fujiAcres.slice(0, 7)
+    //console.log('fugitons',fujiTons)
+    
+    //for(j = fujiTons[0] + 6.5 * ; j < fujiTons.length, j++
+    
+
+
+let galaTons = galaAcres.slice(0,7)
+    //console.log('galatons', galaTons)
+
+let pinkTons = pinkAcres.slice(0,7)
+    //console.log('pinktons', pinkTons)
+
+// Wasn't sure how to complete this one and ended up just writing on paper and using a calculator. 
+
+
+
+
+// PROBLEM 5
+
+/*
+    Next, calculate the total number of 
+    pounds picked per variety.
+
+    You'll need to add up the tons per
+    each variety and convert the number 
+    into pounds -- store that number in
+    the variables given below. 
+
+    Log each of the values to the console.
+
+    Hint: there are 2000 pounds in a ton.
+*/
+
+// CODE HERE 
+
+//used calculator
+
+ let fujiPounds = 214289.3
+ //97.2 Tons
+ let galaPounds = 372581
+ //169 Tons
+ let pinkPounds = 315481.5
+ //143.1 Tons
+
+
+
+
+
+
+// PROBLEM 6
+
+/*
+    Now that you know the total pounds
+    per variety, use the prices given 
+    at the beginning of this file to 
+    figure out how much you'll make 
+    from selling each type of apple. 
+
+    The prices are per pound and are 
+    written in cents. 
+
+    Log each of the profits to the 
+    console. 
+*/
+const fujiPrice = .89 
+const galaPrice = .64
+const pinkPrice = .55
+// CODE HERE
+
+let fugiProfit = fujiPounds * .89
+    console.log(fugiProfit)
+
+let galaProfit = galaPounds * .64
+    console.log(fugiProfit) 
+    
+let pinkProfit = pinkPounds * 55
+    console.log(pinkProfit)
+
+// let galaProfit =
+// let pinkProfit =
+//let fujiProfit = 
+
+
+
+
+
+// PROBLEM 7
+
+/*
+    Add up all your profits and save 
+    the number to a variable called 
+    `totalProfit`.
+
+    Log `totalProfit` to the console.
+*/
+
+// CODE HERE
+let totalProfit = pinkProfit + fugiProfit + galaProfit
+console.log(totalProfit)
---
+++ sdlc.js
@@ -0,0 +1,35 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+//1. Planning
+//2. Ananlysis of Requirements
+//3. Design 
+//4. Implementation
+//5. Testing and Integration
+//6. Maintain
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+let Planning = 'This is the step in which you establish your end goals, language, and features'
+let AnanlysisofRequirements = 'This is the step in which you esablish and study which tools will be required for your project to run properly.'
+let Design = 'This is the step in which you decide what you want your project to look like and how you shoule layout your code.'
+let Implementation = 'This is the step in which you get to code!'
+let Maintain = 'This is the step in which you have to begin fixing bugs and ensuring that your project works. You must constantly maintain your project to ensure that it stays in good condition.'
+
