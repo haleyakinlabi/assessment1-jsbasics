b477820f10206382cb6865f0b4617e5f409c779c
Haley Akin <wasatchtimpanogos@outlook.com>
2022-10-01 09:08:22 -0600

    old and new test answers

--- sdlc.js
+++ sdlc.js
@@ -12,24 +12,31 @@
 //5. Testing and Integration
 //6. Maintain
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
-// Planning
-
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 
-let Planning = 'This is the step in which you establish your end goals, language, and features'
-let AnanlysisofRequirements = 'This is the step in which you esablish and study which tools will be required for your project to run properly.'
-let Design = 'This is the step in which you decide what you want your project to look like and how you shoule layout your code.'
-let Implementation = 'This is the step in which you get to code!'
-let Maintain = 'This is the step in which you have to begin fixing bugs and ensuring that your project works. You must constantly maintain your project to ensure that it stays in good condition.'
+
+//New Answer: 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const ananlysisOfRequirements = 'Determining what the key needs/features/goals that our product will require.' 
+const design = 'Models the way a software application will work, and what technologies should be used to meet the requirements.'
+const implementation = 'Carrying out the designs. This is the step in which you get to code!!!'
+const testingAndIntergration = 'Ensures the product works as designed and in the intended environments.'
+const maintain = 'Ensure that the product is still working/staying up to date as well as adding new features.'
+
+
+
+
+//Original Answer: 
+
+//let Planning = 'This is the step in which you establish your end goals, language, and features'
+//let AnanlysisofRequirements = 'This is the step in which you esablish and study which tools will be required for your project to run properly.'
+//let Design = 'This is the step in which you decide what you want your project to look like and how you shoule layout your code.'
+//let Implementation = 'This is the step in which you get to code!'
+//let TestingandIntergration = 'making sure the code works.' 
+//let Maintain = 'This is the step in which you have to begin fixing bugs and ensuring that your project works. You must constantly maintain your project to ensure that it stays in good condition.'
 
